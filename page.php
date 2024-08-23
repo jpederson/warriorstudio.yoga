c8689f27f7dfe5f0435b5d20a1c9f0653217c935
James Pederson <james@jpederson.com>
2024-08-23 23:56:56 +0000

    Move store content to normal template with logic.

--- page.php
+++ page.php
@@ -2,7 +2,32 @@
 
 get_header();
 
-the_components();
+// if this is a woocommerce page, we need the traditional WordPress loop
+if ( is_woocommerce() || is_cart() || is_checkout() ):
+    ?>
+
+<div class="one-column-container pad-tall bg-white">
+	<div class="wrap">
+	<?php 
+	if ( have_posts() ) :
+		while ( have_posts() ) : the_post(); 
+			?>
+		<h1><?php the_title(); ?></h1>
+		<?php the_content(); ?>
+			<?php
+		endwhile;
+	endif;
+	?>
+	</div>
+</div>
+
+    <?php
+else:
+
+    // if it's not in the store, we're using ACF components.
+    the_components();
+
+endif;
 
 get_footer();
 
